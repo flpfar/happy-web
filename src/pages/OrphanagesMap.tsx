1ec42293e5d433162d0ed4a01124584371ec1945
Felipe Rosa <flp.far@gmail.com>
2020-10-16 08:51:46 -0300

    Add marker on map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css';
-import mapMarking from '../images/map-marker.svg';
+import mapMarkerImg from '../images/map-marker.svg';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarking} alt="Happy Marking"/>
+          <img src={mapMarkerImg} alt="Happy Marking"/>
           <h2>Choose an orphanage in the map</h2>
           <p>A lot of children are waiting for your visit :)</p>
         </header>
@@ -30,8 +38,21 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer 
-          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
         />
+
+        <Marker 
+          icon={mapIcon}
+          position={[-21.8678323, -47.4747237]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Some point
+            <Link to="">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+
       </Map>
 
       <Link to="" className="create-orphanage">
